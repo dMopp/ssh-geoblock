f69598a742d2e957e10f2f6053eb5508eaa0c8bd
Daniel Q <daniel@familie-quolke.de>
2024-11-12 18:07:18 +0100

    Ugly extended with networks

--- sshCountryFilter.sh
+++ sshCountryFilter.sh
@@ -2,10 +2,13 @@
 
 # UPPERCASE space-separated country codes to ACCEPT
 ALLOW_COUNTRIES="DE"
-WHITELIST_IPS=(192.168.1.54 192.168.1.1)
+WHITELIST_IPS=(192.168.1.12 fd11:192:168:1::12)
+
+###Only /24 /128
+WHITELIST_NETWORKS=(fd11:192:168:2:: 192.168.2.)
 
 if [ $# -ne 1 ]; then
-  echo "Usage: `basename $0` <ip>" 1>&2
+  echo "Usage:  `basename $0` <ip>" 1>&2
   exit 0 # return true in case of config issue
 fi
 
@@ -17,12 +20,23 @@ fi
 
 for IP in ${WHITELIST_IPS[@]}; do
         if [ $1 == $IP ]; then
-                RESPONSE="ALLOW WHITELIST"
-                logger "$RESPONSE sshd connection from $1 (WHITELIST)"
+                RESPONSE="ALLOW WHITELIST IP"
+                logger "$RESPONSE sshd connection from $1"
                 exit 0
         fi
 done
 
+for NUMBER in {1..254}; do
+        for NETWORK in ${WHITELIST_NETWORKS[@]}; do
+                for IP in $(echo "${NETWORK}${NUMBER}"); do
+                        if [ $1 == $IP ]; then
+                                RESPONSE="ALLOW WHITELIST NETWORK"
+                                logger "$RESPONSE sshd connection from $1"
+                                exit 0
+                        fi
+                done
+        done
+done
 
 COUNTRY=$($geoiplookupbin $1 | awk -F ": " '{ print $2 }' | awk -F "," '{ print $1 }' | head -n 1)
 
@@ -33,6 +47,6 @@ then
   logger "$RESPONSE sshd connection from $1 ($COUNTRY)"
   exit 0
 else
-  logger "$RESPONSE sshd connection from $1 ($COUNTRY)"
+  logger "GEOBLOCKED sshd connection from $1 ($COUNTRY)" #Fail2Ban friendly
   exit 1
 fi
